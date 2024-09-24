4a48f6255cfe31213ae04d460144cd18d887ffb6
ruben1399@outlook.com <ruben1399@outlook.com>
2024-09-24 18:44:47 +0400

    commit

---
+++ MyC#49/Irepasiroti/IRepasitori.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyC_49.Irepasiroti
+{
+    internal interface IRepasitori<T>
+    {
+        void Add(T t);
+        T Find(int Id);
+        List<T> FindAll();
+        void Update(T t); 
+        void Delete(int Id);
+    }
+}
---
+++ MyC#49/Transishn/Cantainer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyC_49.Transishn
+{
+    internal class Cantainer
+    {
+        //public Cantainer(int id, bool open, decimal caficent)
+        //{
+        //    Id = id;
+        //    Open = open;
+        //    Caficent = caficent;
+        //}
+
+        public int Id { get; set; }
+        public bool Open { get; set; }
+        public decimal Caficent { get; set; }
+
+    }
+}
