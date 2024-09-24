8f1f371c181db24e158618f1764a558fbeecae49
ruben1399@outlook.com <ruben1399@outlook.com>
2024-09-24 19:16:00 +0400

    ok

---
+++ MyC#49/Transishn/CarType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyC_49.Transishn
+{
+    public class CarType
+    {
+        public CarType()
+        {
+        }
+
+        public CarType(int id, string name, string type, decimal caficent)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+            Caficent = caficent;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public decimal Caficent { get; set; }
+
+    }
+}
---
+++ MyC#49/repasitori/RepasitoriCarCrash.cs
@@ -0,0 +1,128 @@
+﻿using MyC_49.Irepasiroti;
+using MyC_49.Transishn;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyC_49.repasitori
+{
+    internal class RepasitoriCarCrash : IRepasitori<CrashedCar>
+    {
+        public const string CS = "Data Source=.;Initial Catalog = CarsDb; Integrated Security = True; Encrypt=False";
+        public void Add(CrashedCar t)
+        {
+            using (SqlConnection connection = new SqlConnection(CS))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "Avelacnel (@Id,@CarCrash,@Caficent)";
+                    command.Parameters.Add(new SqlParameter("@Id", t.Id));
+                    command.Parameters.Add(new SqlParameter("@CarCarsh", t.CarCrash));
+                    command.Parameters.Add(new SqlParameter("@Caficent", t.Caficent));
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int Id)
+        {
+            using (SqlConnection connection = new SqlConnection(CS))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "Delete by @id";
+                    command.Parameters.Remove(new SqlParameter("Id", Id));
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public CrashedCar Find(int Id)
+        {
+            using (SqlConnection connection = new SqlConnection(CS))
+            {
+                connection.Open();
+
+                CrashedCar crashedCar = new CrashedCar();
+
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "Find by @Id";
+                    command.Parameters.Add(new SqlParameter("@CarCrash", Id));
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            crashedCar.Id = int.Parse(reader["Id"].ToString());
+                            crashedCar.CarCrash = bool.Parse(reader["CarCrash"].ToString());
+                            crashedCar.Caficent = int.Parse(reader["Caficent"].ToString());
+                        }
+                    }
+                }
+                return crashedCar;
+            }
+        }
+
+        public List<CrashedCar> FindAll()
+        {
+            using (SqlConnection connection = new SqlConnection(CS))
+            {
+                connection.Open();
+
+                List<CrashedCar> crashedCars = new List<CrashedCar>();
+
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "Bolory";
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            CrashedCar crashedCar = new CrashedCar();
+
+                            crashedCar.Id = int.Parse(reader["Id"].ToString());
+                            crashedCar.CarCrash = bool.Parse(reader["CarCrash"].ToString());
+                            crashedCar.Caficent = int.Parse(reader["Caficent"].ToString());
+
+                            crashedCars.Add(crashedCar);
+                        }
+                    }
+                }
+                return crashedCars;
+            }
+        }
+
+        public void Update(CrashedCar t)
+        {
+            using(SqlConnection connection = new SqlConnection(CS))
+            {
+                connection.Open();
+
+                using(SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "All informacia by = @Id";
+                    command.Parameters.Add(new SqlParameter("@CarCrash", t.CarCrash));
+                    command.Parameters.Add(new SqlParameter("@Caficent",t.Caficent));
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
